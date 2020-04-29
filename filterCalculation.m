382d77d48f86fd676571ba563d27c5902f336347
Sam <saimouer@gmail.com>
2020-04-29 03:04:45 -0400

    Problem is in the filter

--- filterCalculation.m
+++ filterCalculation.m
@@ -3,12 +3,26 @@
 
 Tss = 2.5*10^-6; %sample
 
-P = 10e3; %rated active power?
+P = 100e3; %rated active power?
 U=480; %inverter phase to phase voltage
 f = 60; % grid freq
 fsw = 5e3; %switching frequency
 
-Cfmax = (0.05*P)/(2*pi*f*U^2);
+%Cfmax = (0.005*P)/(2*pi*f*U^2);
+%Cfmax = 1e-9
+%Lf = (0.1*U^2)/(2*pi*f*P); % filter inductance
+%RLf = 100*Lf; % filter resistance
 
-Lf = (0.1*U^2)/(2*pi*f*P); % filter inductance
-RLf = Lf*100; % filter resistance
+%%%%%%%%%%%%
+% Quality factory
+
+
+
+%calculate quality factor
+q = (480*sqrt(2))/720;
+
+RLf = 1
+Lf = 1/(2*pi*60*q);
+Cfmax = (1/L)*(1/(2*pi*60))^2;
+
+
