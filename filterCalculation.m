012931652aac6079bd700d8daab8a7016b50ec50
Sam <saimouer@gmail.com>
2020-05-04 14:36:59 -0400

    finishing up the report

--- filterCalculation.m
+++ filterCalculation.m
@@ -4,7 +4,7 @@
 Tss = 2.5*10^-6; %sample
 
 P = 80e3; %rated active power?
-U=380; %inverter phase to phase voltage
+U=480; %inverter phase to phase voltage
 f = 60; % grid freq
 fsw = 5e3; %switching frequency
 
@@ -12,17 +12,5 @@ Cfmax = (0.05*P)/(2*pi*f*U^2);
 %Cfmax = 1e-9
 Lf = (0.1*U^2)/(2*pi*f*P); % filter inductance
 RLf = 100*Lf; % filter resistance
-
-%%%%%%%%%%%%    
-% Quality factory
-
-
-
-%calculate quality factor
-%q = (480*sqrt(2))/720;
-
-%RLf = 1
-%Lf = 1/(2*pi*60*q);
-%Cfmax = (1/L)*(1/(2*pi*60))^2;
 
 
